bb1951985222eef5e8cab48304ab808364e7d8c7
Anthony <anthony.ferraro@digitalsurgeons.com>
2021-06-30 20:14:08 -0400

    mobile updates

--- pages/index.js
+++ pages/index.js
@@ -14,12 +14,12 @@ export default function Home() {
           }
         `}
       </style>
-      <nav className="flex justify-between mx-2 mt-3 lg:mx-25 lg:mt-25 mb-175 h-14">
+      <nav className="flex justify-between mx-2 mt-3 mb-8 lg:mx-25 lg:mt-25 lg:mb-175 h-14">
         <Coffee />
         <div className="flex flex-row ">
           <a
             href="https://github.com/a-ferraro007"
-            className="self-end font-medium font-display text-primary-text lg:text-3xl lg:mr-10"
+            className="self-end mr-3 text-3xl font-medium font-display text-primary-text lg:mr-10"
             target="_blank"
             rel="noreferrer"
             >
@@ -30,24 +30,24 @@ export default function Home() {
         </div>
       </nav>
       <main>
-        <div className="flex flex-col-reverse lg:justify-center lg:flex-row">
+        <div className="flex flex-col-reverse px-10 lg:justify-center lg:flex-row">
           <div className="mb-12 lg:self-center lg:mr-28 lg:max-w-528 "
           >
-            <h1 className="font-semibold font-display text-primary-text lg:text-64">
+            <h1 className="text-4xl font-semibold font-display text-primary-text lg:text-64">
               {' '}
               Anthony Ferraro
             </h1>
-            <span className="text-2xl text-primary-text font-secondary">
+            <span className="text-lg lg:text-2xl text-primary-text font-secondary">
               {' '}
               Frontend Engineer
             </span>
-            <p className="mt-4 mb-8 text-3xl font-medium font-display text-primary-text">
+            <p className="mt-4 mb-8 text-xl font-medium lg:text-3xl font-display text-primary-text">
               {' '}
-              {`Hi 👋, I'm Anthony. Frontend Engineer from New Haven, CT. Currently
+              {`Hi 👋, I'm Anthony, a Frontend Engineer from New Haven, CT. Currently
               working at Digital Surgeons.`}{' '}
             </p>
             <a
-              href="mailto:adferra24@gmail.com" className="px-8 py-2 transition duration-500 border-2 hover:bg-transparent font-display text-primary-light bg-primary-text rounded-img hover:text-primary-text border-primary-text"
+              href="mailto:adferra24@gmail.com" className="px-8 py-2 font-semibold transition duration-300 border-2 hover:bg-transparent font-display text-primary-light bg-primary-text rounded-img hover:text-primary-text border-primary-text"
               target="_blank"
               rel="noreferrer"
             >
@@ -56,7 +56,7 @@ export default function Home() {
             </a>
           </div>
 
-          <div className="w-52 lg:w-96">
+          <div className="self-center mb-10 w-52 md:w-72 lg:w-96 lg:mb-0">
             <Image
               className="border-8 border-blue-400 border-dashed rounded-img img-border"
               alt="Overly dramatic picture of me eating a cookie."
