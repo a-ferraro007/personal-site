c2945459a49f57166c265da595e8edaeb2d8a209
Anthony <anthony.ferraro@digitalsurgeons.com>
2021-07-06 12:25:44 -0400

    quick overflow hidden updateto remove scroll bar when resizing the page

--- pages/_document.js
+++ pages/_document.js
@@ -21,7 +21,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
         </Head>
-        <body className="px-3 lg:px-10 bg-primary-light dark:bg-primary-dark-mode relative">
+        <body className="relative px-3 overflow-hidden lg:px-10 bg-primary-light dark:bg-primary-dark-mode">
           <Main />
           <NextScript />
         </body>
