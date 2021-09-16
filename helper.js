b96b7bc3e8c310c8a829bb22bcd276b68c87dace
Anthony Ferraro <adferra24@gmail.com>
2021-09-15 23:58:16 -0400

    making everything dark mode by defualt

--- helper.js
+++ helper.js
@@ -1,12 +1,13 @@
 export const getInitialTheme = () => {
   if (process.browser) {
-    if (
-      window.localStorage.theme === 'true' ||
-      window.matchMedia('(prefers-color-scheme: dark)').matches
-    ) {
-      return { darkMode: true }
-    } else {
-      return { darkMode: false }
-    }
+    //if (
+    //  window.localStorage.theme === 'true' ||
+    //  window.matchMedia('(prefers-color-scheme: dark)').matches
+    //) {
+    //  return { darkMode: true }
+    //} else {
+    //  return { darkMode: false }
+    //}
+    return { darkMode: true }
   }
 }
